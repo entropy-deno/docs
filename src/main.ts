829433fea83c1a3cec01fb8b4d827ba40ca6a0f6
dominiq007 <dom.rajkowski@gmail.com>
2023-08-14 11:40:37 +0200

    fix: dev env hostname

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
+import { Configurator } from '@entropy/configurator';
 import { createServer } from '@entropy/server';
+import { inject } from '@entropy/injector';
+
 import { RootModule } from './root.module.ts';
 import { BadgeModule } from './badges/badge.module.ts';
 
 if (import.meta.main) {
+  const configurator = inject(Configurator);
+
   const server = createServer({
     config: {
       contentSecurityPolicy: {
@@ -11,7 +16,7 @@ if (import.meta.main) {
           'https://*.clarity.ms',
         ],
       },
-      host: 'entropy.deno.dev',
+      host: configurator.entries.isProduction ? 'entropy.deno.dev' : 'localhost',
     },
     modules: [
       RootModule,
