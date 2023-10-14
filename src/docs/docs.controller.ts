7e3ddbe421008238dd1602efb2c3e0abbd5d34ce
dominiq007 <dom.rajkowski@gmail.com>
2023-10-14 19:54:30 +0200

    refactor: move nav links array to controller

--- src/docs/docs.controller.ts
+++ src/docs/docs.controller.ts
@@ -4,12 +4,20 @@ import { Route } from '@entropy/router';
 export class DocsController extends Controller {
   @Route.Get('/docs/:section?/:page?')
   public show(section?: string, page?: string) {
+    const navigationLinks = [
+      ['', 'Getting Started'],
+      ['/essentials/overview', 'Overview'],
+      ['/essentials/configuration', 'Configuration'],
+      ['/essentials/modules', 'Modules'],
+    ];
+
     return this.render(
       !section || !page
         ? 'pages/docs/index'
         : `pages/docs/${section}/${page.replaceAll('-', '_')}`,
       {
         pageTitle: 'Docs',
+        navigationLinks,
       },
     );
   }
