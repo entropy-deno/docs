75b88817d282d0574b5452092133648655b05665
dominiq007 <dom.rajkowski@gmail.com>
2023-10-14 19:54:56 +0200

    feat: remove badge module

--- src/badges/badge.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@entropy/server';
-import { BadgeController } from './badge.controller.ts';
-
-export class BadgeModule implements Module {
-  public readonly controllers = [
-    BadgeController,
-  ];
-}
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import { createServer } from '@entropy/server';
 import { RootModule } from './root.module.ts';
 import { DocsModule } from './docs/docs.module.ts';
-import { BadgeModule } from './badges/badge.module.ts';
 
 if (import.meta.main) {
   const server = createServer({
@@ -20,7 +19,6 @@ if (import.meta.main) {
     modules: [
       RootModule,
       DocsModule,
-      BadgeModule,
     ],
   });
 
