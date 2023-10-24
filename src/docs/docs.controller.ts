06fff3845bea5a3f55042967d18213253bc84b82
dominiq007 <dom.rajkowski@gmail.com>
2023-10-24 20:08:38 +0200

    feat: upgrade framework

--- src/docs/docs.controller.ts
+++ src/docs/docs.controller.ts
@@ -1,17 +1,19 @@
-import { Controller } from '@entropy/http';
+import { Controller, Transform } from '@entropy/http';
 import { inject } from '@entropy/injector';
+import { SnakeCasePipe } from '@entropy/pipes';
 import { Route } from '@entropy/router';
 import { LinkService } from '../links/link.service.ts';
 
 export class DocsController extends Controller {
   private readonly linkService = inject(LinkService);
 
+  @Transform({
+    'page': SnakeCasePipe,
+  })
   @Route.Get('/docs/:section?/:page?')
   public show(section?: string, page?: string) {
     return this.render(
-      !section || !page
-        ? 'pages/docs/index'
-        : `pages/docs/${section}/${page.replaceAll('-', '_')}`,
+      !section || !page ? 'pages/docs/index' : `pages/docs/${section}/${page}`,
       {
         pageTitle: 'Docs',
         navigationLinks: this.linkService.getNavigationLinks(),
