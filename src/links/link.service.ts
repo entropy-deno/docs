b9cc12d20652046f3c8406ec18a7919911950255
dominiq007 <dom.rajkowski@gmail.com>
2023-11-25 16:05:55 +0100

    docs: add http client and localization sections

--- src/links/link.service.ts
+++ src/links/link.service.ts
@@ -1,16 +1,22 @@
 export class LinkService {
   public getNavigationLinks() {
-    return [
-      ['', 'Getting Started'],
-      ['/essentials/overview', 'Overview'],
-      ['/essentials/configuration', 'Configuration'],
-      ['/essentials/modules', 'Modules'],
-      ['/essentials/controllers-and-routes', 'Controllers and Routes'],
-      ['/essentials/requests', 'Requests'],
-      ['/essentials/views', 'Views'],
-      ['/essentials/dependency-injection', 'Dependency Injection'],
-      ['/essentials/session', 'Session'],
-      ['/essentials/middleware', 'Middleware'],
-    ];
+    return {
+      essentials: [
+        ['', 'Getting Started'],
+        ['/essentials/overview', 'Overview'],
+        ['/essentials/configuration', 'Configuration'],
+        ['/essentials/modules', 'Modules'],
+        ['/essentials/controllers-and-routes', 'Controllers and Routes'],
+        ['/essentials/requests', 'Requests'],
+        ['/essentials/views', 'Views'],
+        ['/essentials/dependency-injection', 'Dependency Injection'],
+        ['/essentials/session', 'Session'],
+        ['/essentials/middleware', 'Middleware'],
+      ],
+      advanced: [
+        ['/advanced/http-client', 'HTTP Client'],
+        ['/advanced/localization', 'Localization'],
+      ],
+    };
   }
 }
